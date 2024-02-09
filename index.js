3d2c4385be56399e5253ad5ca1e43de1c4adcf0f
gary <garyliggins@yahoo.com>
2024-02-08 22:04:04 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (names) => {
+    let newNames = names.slice(0,2);
+    return newNames;
+}
+
+const returnLastTwoDrivers = (lastNames) => {
+    let newLastNames = lastNames.slice(-2)
+    return newLastNames;
+}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = (num) => {
+    
+    return function faremultiply(fare){
+       return fare * num;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+
+const selectDifferentDrivers = (arrayOfDrivers,func) => {
+    return func(arrayOfDrivers);
+}
